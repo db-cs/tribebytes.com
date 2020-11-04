7a89111d0ef8d4d0ecfce639dd3b6b9a50786ab5
Nathan Bailey <dbailey@k12k.com>
2020-11-04 12:34:33 -0500

    Add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>
+      <h1 className="text-6xl">
         Get started by editing <code>pages/index.js</code>
       </h1>
-    </div>
+    </>
   );
 }
