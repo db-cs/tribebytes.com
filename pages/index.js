25324d704869d9cf47bb335882f26528830399a4
Nathan Bailey <dbailey@k12k.com>
2020-11-04 07:58:01 -0500

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>
+        Get started by editing <code>pages/index.js</code>
+      </h1>
+    </div>
+  );
+}
