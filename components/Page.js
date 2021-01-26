026f212b51fb2e13b5d3eaf63b31fcddfd157f2c
evevzav <zavAVzwvwv>
2021-01-26 12:13:40 -0500

    create activelink

--- components/Page.js
+++ components/Page.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import ActiveLink from "./activeLink";
+import Router from "next/router";
+import handleClick from "./activeLink";
 const Header = () => {
   // <body background="https://i.pinimg.com/originals/4a/aa/a3/4aaaa35e178c1cc7c300c53556077d63.jpg">
   return (
@@ -13,52 +14,9 @@ const Header = () => {
           >
             <img src="/Logo.png" height="150" width="150"></img>
           </div>
-          <nav class="mt-5 lg:mt-0 lg:ml-4 ">
-            <div class="flex ">
-              <span class="hidden sm:block ">
-                <Link href="/">
-                  <button
-                    onPress={handleClick}
-                    href="#"
-                    type="button"
-                    className={
-                      router.pathname === href
-                        ? "underline transition duration-300 ease-in customPlugin transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
-                        : "no-underline transition duration-300 ease-in customPlugin transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
-                    }
-                    // class="transition duration-300 ease-in customPlugin transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
-                  >
-                    Home
-                  </button>
-                </Link>
-              </span>
-
-              <span class="hidden sm:block ml-3">
-                <Link href="/about">
-                  <button
-                    onPress={handleClick}
-                    type="button"
-                    class="transition duration-300 ease-in transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
-                  >
-                    About
-                  </button>
-                </Link>
-              </span>
-
-              <span class="sm:ml-3">
-                <button
-                  onPress={handleClick}
-                  type="button"
-                  class="transition duration-300 ease-in transform hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800"
-                >
-                  More
-                </button>
-              </span>
-            </div>
-          </nav>
         </div>
       </header>
     </>
   );
 };
-export default { Header, ActiveLink };
+export default Header;
--- components/activeLink.js
+++ components/activeLink.js
@@ -1,13 +1,51 @@
 import { useRouter } from "next/router";
 
-function ActiveLink({ children, href }) {
-  const router = useRouter();
-
+const ActiveLink = () => {
   const handleClick = (e) => {
+    const router = useRouter();
     e.preventDefault();
     router.push(href);
-    return ()
-    //take button & link component from page and apply in here
   };
-}
+  return (
+    <>
+      <span>
+        <Link href="/">
+          <button
+            onPress={handleClick}
+            className={
+              router.pathname === href
+                ? "underline transition duration-300 ease-in customPlugin transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
+                : "no-underline transition duration-300 ease-in customPlugin transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
+            }
+          >
+            Home
+          </button>
+        </Link>
+      </span>
+
+      <span class="hidden sm:block ml-3">
+        <Link href="/about">
+          <button
+            onPress={handleClick}
+            type="button"
+            class="transition duration-300 ease-in transform active:underline hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800 hover:underline"
+          >
+            About
+          </button>
+        </Link>
+      </span>
+
+      <span class="sm:ml-3">
+        <button
+          onPress={handleClick}
+          type="button"
+          class="transition duration-300 ease-in transform hover:scale-110 inline-flex items-center px-4 py-2 border border-black rounded-md shadow-lg text-sm font-medium text-black bg-transparent hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-800"
+        >
+          More
+        </button>
+      </span>
+    </>
+  );
+};
+
 export default ActiveLink;
