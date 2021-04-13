a7286b076ea7fae19ab72af6d0f84b5af3ecf2d3
evevzav <zavAVzwvwv>
2021-04-13 09:23:40 -0400

    Edit graduates Complete ee

--- components/Graduate.js
+++ components/Graduate.js
@@ -1,11 +1,14 @@
 import Image from "next/image";
+import ee from "../pages/ee";
 
-const Graduate = ({ id, name, year, quote, picture }) => (
+const Graduate = ({ id, name, year, quote, picture, ee }) => (
   <div className="p-5 text-white bg-red-900 border-2 m-5">
     <h1 className="font-semibold text-4xl mb-1">{name}</h1>
     <p className="mb-1">Graduated {year}</p>
     <p className="mb-1 w-48 content-center flex flex-wrap">"{quote}"</p>
-    <Image className="m-5" src={picture} width={200} height={160} />
+    <a href={ee}>
+      <Image className="m-5" src={picture} width={200} height={160} />
+    </a>
   </div>
 );
 
