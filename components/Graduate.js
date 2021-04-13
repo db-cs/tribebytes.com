0f3f425145b88c81bdb4324079693141d6c692bd
Nathan Bailey <dbailey@k12k.com>
2021-04-13 12:23:45 +0000

    Update graduate component using github profile photo

--- components/Graduate.js
+++ components/Graduate.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
 
-const Graduate = ({ id, name, year, quote, picture }) => (
-  <div className="p-5 text-white bg-red-900 border-2 m-5">
-    <h1 className="font-semibold text-4xl mb-1">{name}</h1>
-    <p className="mb-1">Graduated {year}</p>
-    <p className="mb-1 w-48 content-center flex flex-wrap">"{quote}"</p>
-    <Image className="m-5" src={picture} width={200} height={160} />
+const Graduate = ({ id, name, year, quote, githubUsername }) => (
+  <div className="p-5 text-center bg-white border-2 rounded-md">
+    <Image className="m-5 rounded-full" src={`https://github.com/${githubUsername}.png`} alt={`Profile picture for ${name}.`} width={150} height={150} layout="intrinsic" />
+    <h1 className="mb-1 text-4xl font-semibold uppercase">{name}</h1>
+    <h2 className="mb-3 font-bold">Graduated {year}</h2>
+    <hr className="w-2/3 mx-auto border-2 border-red-700" />
+    <p className="mt-3">"{quote}"</p>
+    <a href={`https://github.com/${githubUsername}`}>
+      <div className="inline-flex items-center justify-center px-6 py-2 my-8 border-2 rounded-md hover:bg-gray-400 hover:border-transparent">
+        <img className="pr-2" src="/GitHub-Mark-32px.png" />
+        <p>{githubUsername}</p>
+      </div>
+    </a>
   </div>
 );
 
--- pages/graduates.js
+++ pages/graduates.js
@@ -6,7 +6,10 @@ import Graduate from "../components/Graduate";
 const Graduates = () => (
   <body>
     <Header />
-    <div className="bg-red-900 px-20 flex flex-wrap h-full justify-center">
+    <div className="flex items-center justify-center h-56 text-white bg-red-900">
+      <h1 className="text-4xl font-bold uppercase">Graduates</h1>
+    </div>
+    <div className="grid justify-center h-full grid-cols-3 gap-8 p-20">
       {graduates.map((graduate) => (
         <Graduate key={graduate.id} {...graduate} />
       ))}
