59d0e9af6b1a73863444ea563407f6d0153002e5
evevzav <zavAVzwvwv>
2021-04-26 07:51:07 -0400

    Add Errors to system

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { useTheme } from "next-themes";
 import Head from "next/head";
 import Header from "./Header";
 
@@ -9,14 +10,40 @@ import Header from "./Header";
  *
  */
 const Layout = ({ title, children }) => {
+  const { theme, setTheme } = useTheme("light");
+
+  const handleDarkToggle = () =>
+    theme == "light" ? setTheme("dark") : setTheme("light");
+
   return (
     <>
       <Head>
         <title>{title ? `${title} | TribeBytes` : `TribeBytes`}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header />
-      <main>{children}</main>
+      <div className="relative">
+        <div className="fixed bottom-0 right-0 inline-block w-10 mr-10 align-middle select-none transition duration-200 ease-in">
+          <p>Toggle Dark Mode</p>
+          <input
+            type="checkbox"
+            name="toggle"
+            id="toggle"
+            onChange={handleDarkToggle}
+            className={`${
+              theme === "dark" &&
+              "right-0 bg-red-900 border border-4 border-gray-300"
+            } mb-10 bottom-0 absolute block w-6 h-6 rounded-full bg-gray-300 border-4 border-red-900 appearance-none cursor-pointer`}
+          />
+          <label
+            for="toggle"
+            className={`${
+              theme === "dark" && "right-0 bg-red-900"
+            } toggle-label block mb-10 overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer`}
+          ></label>
+        </div>
+        <Header />
+        <main>{children}</main>
+      </div>
     </>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/index.css";
 import Head from "next/head";
+import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => (
   <>
@@ -10,7 +11,9 @@ const App = ({ Component, pageProps }) => (
         content="TribeBytes is the nickname for our Coding Program of study at Dobyns-Bennett High School. Located in Northeast Tennessee, Dobyns-Bennett serves around 2,300 students. The program includes a variety of courses and extra-curricular activities that are designed to prepare students to work in the Information Technology/Software Development industry."
       ></meta>
     </Head>
-    <Component {...pageProps} />
+    <ThemeProvider attribute="class">
+      <Component {...pageProps} />
+    </ThemeProvider>
   </>
 );
 
