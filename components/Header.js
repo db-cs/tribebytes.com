23d340f106807df538b98affd2cd928d7fa81229
evevzav <zavAVzwvwv>
2021-04-30 10:28:12 -0400

    change ee

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
   return (
     <>
       <header className="dark:bg-black">
+        <a
+          className="opacity-0 cursor-default absolute"
+          href="/FGHJUYTGHJKIUYTGHJUYTGHJUYTFGHJUIYTREDFGHJKOIU7Y65REDFGHJI87654EWSDFGHJKIOI876TRESXCVBNJKIUYTREDSXCVBNJKIUYTRESXCVBNJKIUYTREDSXCVBNMKIUYTREDSXCVBNMKIUYTREDSXCVBNMJKUYTRESXZCVBNMJKUYTREDXC"
+        >
+          .
+        </a>
         <div className="flex max-w-6xl justify-between m-8 mx-auto sm:items-center ">
           <Link href="/">
             <a className="cursor-pointer">
@@ -17,7 +23,6 @@ const Header = () => {
               />
             </a>
           </Link>
-
           <Nav />
         </div>
       </header>
--- pages/FGHJUYTGHJKIUYTGHJUYTGHJUYTFGHJUIYTREDFGHJKOIU7Y65REDFGHJI87654EWSDFGHJKIOI876TRESXCVBNJKIUYTREDSXCVBNJKIUYTRESXCVBNJKIUYTREDSXCVBNMKIUYTREDSXCVBNMKIUYTREDSXCVBNMJKUYTRESXZCVBNMJKUYTREDXC.js
+++ pages/FGHJUYTGHJKIUYTGHJUYTGHJUYTFGHJUIYTREDFGHJKOIU7Y65REDFGHJI87654EWSDFGHJKIOI876TRESXCVBNJKIUYTREDSXCVBNJKIUYTRESXCVBNJKIUYTREDSXCVBNMKIUYTREDSXCVBNMKIUYTREDSXCVBNMJKUYTRESXZCVBNMJKUYTREDXC.js
@@ -1,38 +1,32 @@
-import Header from "../components/Header";
+import Layout from "../components/Layout";
 
 export default function FGHJUYTGHJKIUYTGHJUYTGHJUYTFGHJUIYTREDFGHJKOIU7Y65REDFGHJI87654EWSDFGHJKIOI876TRESXCVBNJKIUYTREDSXCVBNJKIUYTRESXCVBNJKIUYTREDSXCVBNMKIUYTREDSXCVBNMKIUYTREDSXCVBNMJKUYTRESXZCVBNMJKUYTREDXC() {
   return (
     <body className="bg-red-900">
-      <div className="absolute animate-bounce">
-        <div className="animate-spin">
-          <img className="w-screen" src="mcbailey, nathan.png" rel="Yes" />
-        </div>
-      </div>
-      <div className="absolute animate-ping">
-        <img className="w-screen" src="Logina.JPG" rel="Yes2" />
-      </div>
-      <h1 className="absolute m-20 text-6xl text-center animate-ping rotate-6">
-        R̶̤͙̭͚̙̯͖͈̜̯͂̐̓̈́̎͘E̶̢̝͎͇̠̫̖͇̘͆̈́͆͊͘T̸̙̻̪͋U̵̡͈̫͇̤͂̊̓Ŗ̸̖͉̥͈̲͍̮̒̈̈N̸̺͍̽̓̇ ̸̘̗̘̭̝̂̀͋̐̀̆͊̏ͅT̷͔̪͖̥̎̇̓̚̚͝O̴̦̓̿̌ ̶̭͇͖̦̩̫̦̹͝P̸̡̟̟̤̮̻͂̋͂Ȑ̸̢̤͎͖̇͊E̸̡̲͕̳̻̖͕͈̾͐̇V̷̢̦̳͔̙̤͚̞̉́͒͛I̷͔̯̪̦̣̞̋͜Ǒ̵̪͕̗̼̘̓́̏͌͌͛́͠U̵̪̪̠̇̀Ś̷̢̧̫̗͇͓̣̩̀͝ ̴̨̨͉͓̫̲̳͇̭̇̐͆́̔̿ͅP̵̨͉̯̲̦̾̄́̇̍̏̈͆̈̂Ą̶̨̳͎͔̣̹̔Ģ̷̮̰̃͊̊̿͘E̶̙͋̿́̿͒̈̈́͘̕
-      </h1>
-      <h1 className="absolute m-64 text-6xl text-center -rotate-45 animate-ping">
-        G̷̮̫̹̖̤̗̩̙̹͛̿̈́ͅO̸̹̳͎͔̭̝̥͛̑̄́̒̓̎̚ ̵̨̹̰̱̣̪̭̀̐̌̄̂̅͒͋͜B̸̟̝̰̲̦̱̯̬̪̳̾͆͆͒̾̀̄̓̕A̸̡̦̻̲̞̬͐̃̌C̶̢̭̜̖̝̲̮͍̼̬̿Ǩ̵̹̎̏̈̊̒̒
-      </h1>
-      <h1 className="relative m-64 text-6xl text-center -rotate-45 animate-ping">
-        Y̶͉̾̏͌͠O̸̧̱̺̯̼̓̓͊͛̎U̸̡̺̭̜͑͆̽̚R̴̡̭̤͈͌̍̀̀͋̈́̃͆̚̚E̶̖͝ ̸͎̹͉̝̘͉͓̣͓͓̂̿̃͛͌N̷̨̺͍̭͖̦̯̯̿̽̽͝Ǫ̴̥͔͍͍͚́ͅT̵̙̪͔͓͈͚̜͒̑̇̇̽̚̕͜ ̴̰̰̑̔͊̈́́̑̌̃͝W̷̩̟͆͂̉̅̐̀́̋̾Ë̸̗̱̱́̽̔̄̓́́L̴͎̥̜͇̹̰̣̾̽͑́̄Ç̷̧̨̹͎̫͎̼͖͙͑̓̆̂̔̀́̚Ỡ̴̬̣̏̇̿͂͛͑M̸̳̬̖̪̑̏͗̈́̓͒ͅE̴̲͈̭̞̣̍̃̓̉̊͒̈͗̽͝ ̷͖͍͖̟̺̀̀H̷̡̛̠̠̖̫͒̿̎͐̆̿̚E̴͙̝̱͍͇͍̩͐̄Ȓ̴̛̖̝̮͛̄̓̇͆̊͝ͅE̷̮͕͈̓̾͗ͅ
-      </h1>
-      <h1 className="relative mr-40 text-6xl text-center -rotate-45 animate-ping">
-        Ć̷͍͙͙̭̾͊̐̏Ơ̴̞͚̘͙̳̼̮͆̋̄̿D̵̗̪̟̮͎͈̎̔̉͑́̈́̚̚͜I̶̢̺͈͕̣͉̼̲̞̞͒́̑̓̎͘̚͝͝N̷̡̺͎̬͖̙̜̅́̀̒̓͠ͅG̵͖͓͂̃̀͐̀̓́̄͛́
-      </h1>
-      <h1 className="relative ml-64 text-6xl text-center -rotate-45 animate-ping">
-        Ć̷͍͙͙̭̾͊̐̏Ơ̴̞͚̘͙̳̼̮͆̋̄̿D̵̗̪̟̮͎͈̎̔̉͑́̈́̚̚͜I̶̢̺͈͕̣͉̼̲̞̞͒́̑̓̎͘̚͝͝N̷̡̺͎̬͖̙̜̅́̀̒̓͠ͅG̵͖͓͂̃̀͐̀̓́̄͛́
-      </h1>
-      <div className="animate-ping">
+      <Layout title="G̵̡̨̢̡̨̛̛̛̮̭̦̤͓̘͎̠̖͎̜͉̬̼̱̺̥̖̪̖̬̻͚̭͙̻̳͖͍̝̥̱̩̥̗̳̥͖̦̪͓͚̞̼̳̈́́͂̐́͑͌͑͐̆͌̾͆͊̊̃͊̍̐̈͛͛͂̉̔̽̄̂̽̾̐̄̊͌̿̀́͐̔͑̐̈̂̓̔̈̓̀̄͒͊̓͐͊͆̆́̓̒̒̒̓̂̃̀̌̑͆̈͊̓͐̚̚͘͝͝͠͠͝͝͝͝Ơ̶̧̮̙̣͎͔̳̯̓́̿̍̀̈́̄̾̓̆̊́͋̎̓͗͆̂͂̓̾͊̽͛͑̈́́̂̂̍̀̀͋̇͛̅͐͐̾͆̍͛̅̄̄͐̍͐̍̎̈̀̓̋̑̌͂͌̏͛͛̈́̈͘̕͘̚̚̕͠ ̸̨̢̢̡̛͖͖͙̥̰̟̹̰͖̖̙̩̬̗̖͈͔̜̖͇͓̖̟̻̣̤̻̲͇̦͉̘̐͐̔͒͌̓̀͆̑̃͂̽͛̍̆̔̉̃͑͊́͛͑̇̓̓͛͋͑̉͌͛̓͐͆͑̌͒̄̎̉̊̐̆͌͛̿̍̉̆̽́͘̕̚͜͠͝͠͝͝A̶̧̨̢̛̹͔̗͓͎̜̻̪̦͖͚̠̻̝͎̜̲̝̖͖̪̲̥͍͈͓͙̲͍̦̠̟̥͓̭̦̥͂̔̀̈́̈́͐̇̔̓̎͗̆̑̃̄̒̅̀̏͐̅̅̇̐͌̇̅̾̊̀́̚̕̚͘͘͠͝͝ͅW̸̧̢̹̞̤̣̟̱͍͖̙̩̠̹̮̫͚̞̻͕̺͓̻̦̠̯̙̺̱̖̺̜͑͆̃̇̊͐̕͘͜͜͝͝A̷̡̛̛͈̜̘͚̭̫͛͊́͂̍̔̅̀̓̽̈́͛͛̓̍̿̽͐̆̊͐̋̍͐̾́͒̀͋̀͂̏́̓̄̃͒͒͑̃̅͑̀͘̚͘̕͠͠Y̷̽̑̈́̽̌͆̈̽̏͆̅̈́̑̓̓̾͒͋̇͌̚̕͘͝ͅ">
+        <h1 className="absolute m-20 text-6xl text-center animate-ping rotate-6">
+          R̶̤͙̭͚̙̯͖͈̜̯͂̐̓̈́̎͘E̶̢̝͎͇̠̫̖͇̘͆̈́͆͊͘T̸̙̻̪͋U̵̡͈̫͇̤͂̊̓Ŗ̸̖͉̥͈̲͍̮̒̈̈N̸̺͍̽̓̇ ̸̘̗̘̭̝̂̀͋̐̀̆͊̏ͅT̷͔̪͖̥̎̇̓̚̚͝O̴̦̓̿̌ ̶̭͇͖̦̩̫̦̹͝P̸̡̟̟̤̮̻͂̋͂Ȑ̸̢̤͎͖̇͊E̸̡̲͕̳̻̖͕͈̾͐̇V̷̢̦̳͔̙̤͚̞̉́͒͛I̷͔̯̪̦̣̞̋͜Ǒ̵̪͕̗̼̘̓́̏͌͌͛́͠U̵̪̪̠̇̀Ś̷̢̧̫̗͇͓̣̩̀͝ ̴̨̨͉͓̫̲̳͇̭̇̐͆́̔̿ͅP̵̨͉̯̲̦̾̄́̇̍̏̈͆̈̂Ą̶̨̳͎͔̣̹̔Ģ̷̮̰̃͊̊̿͘E̶̙͋̿́̿͒̈̈́͘̕
+        </h1>
+        <h1 className="absolute m-64 text-6xl text-center -rotate-45 animate-ping">
+          G̷̮̫̹̖̤̗̩̙̹͛̿̈́ͅO̸̹̳͎͔̭̝̥͛̑̄́̒̓̎̚ ̵̨̹̰̱̣̪̭̀̐̌̄̂̅͒͋͜B̸̟̝̰̲̦̱̯̬̪̳̾͆͆͒̾̀̄̓̕A̸̡̦̻̲̞̬͐̃̌C̶̢̭̜̖̝̲̮͍̼̬̿Ǩ̵̹̎̏̈̊̒̒
+        </h1>
+        <h1 className="absolute m-64 text-6xl text-center -rotate-45 animate-ping">
+          Y̶͉̾̏͌͠O̸̧̱̺̯̼̓̓͊͛̎U̸̡̺̭̜͑͆̽̚R̴̡̭̤͈͌̍̀̀͋̈́̃͆̚̚E̶̖͝ ̸͎̹͉̝̘͉͓̣͓͓̂̿̃͛͌N̷̨̺͍̭͖̦̯̯̿̽̽͝Ǫ̴̥͔͍͍͚́ͅT̵̙̪͔͓͈͚̜͒̑̇̇̽̚̕͜ ̴̰̰̑̔͊̈́́̑̌̃͝W̷̩̟͆͂̉̅̐̀́̋̾Ë̸̗̱̱́̽̔̄̓́́L̴͎̥̜͇̹̰̣̾̽͑́̄Ç̷̧̨̹͎̫͎̼͖͙͑̓̆̂̔̀́̚Ỡ̴̬̣̏̇̿͂͛͑M̸̳̬̖̪̑̏͗̈́̓͒ͅE̴̲͈̭̞̣̍̃̓̉̊͒̈͗̽͝ ̷͖͍͖̟̺̀̀H̷̡̛̠̠̖̫͒̿̎͐̆̿̚E̴͙̝̱͍͇͍̩͐̄Ȓ̴̛̖̝̮͛̄̓̇͆̊͝ͅE̷̮͕͈̓̾͗ͅ
+        </h1>
+        <h1 className="relative mr-40 text-6xl text-center -rotate-45 animate-ping">
+          Ć̷͍͙͙̭̾͊̐̏Ơ̴̞͚̘͙̳̼̮͆̋̄̿D̵̗̪̟̮͎͈̎̔̉͑́̈́̚̚͜I̶̢̺͈͕̣͉̼̲̞̞͒́̑̓̎͘̚͝͝N̷̡̺͎̬͖̙̜̅́̀̒̓͠ͅG̵͖͓͂̃̀͐̀̓́̄͛́
+        </h1>
+        <h1 className="relative ml-64 text-6xl text-center -rotate-45 animate-ping">
+          Ć̷͍͙͙̭̾͊̐̏Ơ̴̞͚̘͙̳̼̮͆̋̄̿D̵̗̪̟̮͎͈̎̔̉͑́̈́̚̚͜I̶̢̺͈͕̣͉̼̲̞̞͒́̑̓̎͘̚͝͝N̷̡̺͎̬͖̙̜̅́̀̒̓͠ͅG̵͖͓͂̃̀͐̀̓́̄͛́
+        </h1>
         <img
-          className="w-screen"
-          scr="CD0A706A-CED9-4A36-9AE7-6F52D3CA47EE.JPG"
-          rel="Yes2"
+          className="animate-spin absolute animate-ping w-screen"
+          src="mcbailey, nathan.png"
+          rel="Yes"
         />
-      </div>
+        <h1 className="absolute text-6xl fixed">Logina</h1>
+        <img className="" src="Logina.JPG" rel="Yes2" />
+      </Layout>
     </body>
   );
 }
